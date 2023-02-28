8a28b8106166fc6d206f9806c49527b9d6272f9e
Ankita <101959776+Asniknigtha@users.noreply.github.com>
2023-02-28 10:59:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,6 +5,13 @@ const connectToMongo = ()=> {
     mongoose.connect(mongoURI, ()=>{
         console.log("connected to mongo successfully")
     })
+    mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+
+    mongoose.connection.db.listCollections().toArray(function (err, names) {
+        console.log(names);
+    });
+})
 }
 
 module.exports = connectToMongo 
